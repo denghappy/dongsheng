aaf16acebd67b03ce508eb7c446b6cb69f7e2552
lele <3343605367@qq.com>
2018-04-15 09:44:33 +0800

    修改复分b部分bug

--- js/main.js
+++ js/main.js
@@ -3,17 +3,6 @@
  */
 //导航栏跟随
 
-/*$(".navbar>ul>li").click(function(){
-    //$(this).addClass("current").siblings().removeClass("current");
-    $(".navbar>ul>li").removeClass("current");
-    $(this).addClass("current");
-});*/
-
-/*$(".navbar ul li a").on("click",function(){
-    $(".navbar ul li a").removeClass("current");
-    $(this).addClass("current");
-})*/
-
  $(".navbar a").each(function(){  
         $this = $(this);  
         if($this[0].href==String(window.location)){  
@@ -31,7 +20,7 @@ $(".toTop").click(function(){
 
 
 //轮播图
-$(function(){
+/*$(function(){
     var i=0;
     //---复制---//
     var clone= $(".banner .banner-img li").eq(0).clone();
@@ -73,7 +62,7 @@ $(function(){
         },2500);
 
     });
-//---单击按钮---//
+    //---单击按钮---//
     $(".banner .banner-btn li").click(function(){
         var index=$(this).index();
         i=index;
@@ -81,11 +70,11 @@ $(function(){
         $(".banner .banner-btn li").eq(i).addClass("focus").siblings().removeClass("focus");
 
     });
-//---鼠标经过时---//
+    //---鼠标经过时---//
     $(".banner").hover(function(){
         $(".banner .prev,.banner .next").fadeToggle();
     });
-//---前进和后退---//
+    //---前进和后退---//
     $(".banner .prev").click(function(){
         i--;
         if(i<0){i=0};
@@ -97,19 +86,60 @@ $(function(){
         move();
     });
 
-});
+});*/
+
+$(document).ready(function () {
+                $(".main_visual").hover(function(){
+                    $("#btn_prev,#btn_next").fadeIn()
+                    },function(){
+                    $("#btn_prev,#btn_next").fadeOut()
+                    })
+                $dragBln = false;
+                $(".main_image").touchSlider({
+                    flexible : true,
+                    speed : 500,
+                    btn_prev : $("#btn_prev"),
+                    btn_next : $("#btn_next"),
+                    paging : $(".flicking_con a"),
+                    counter : function (e) {
+                        $(".flicking_con a").removeClass("on").eq(e.current-1).addClass("on");
+                    }
+                });
+                $(".main_image").bind("mousedown", function() {
+                    $dragBln = false;
+                })
+                $(".main_image").bind("dragstart", function() {
+                    $dragBln = true;
+                })
+                $(".main_image a").click(function() {
+                    if($dragBln) {
+                        return false;
+                    }
+                })
+                timer = setInterval(function() { $("#btn_next").click();}, 5000);
+                $(".main_visual").hover(function() {
+                    clearInterval(timer);
+                }, function() {
+                    timer = setInterval(function() { $("#btn_next").click();}, 5000);
+                })
+                $(".main_image").bind("touchstart", function() {
+                    clearInterval(timer);
+                }).bind("touchend", function() {
+                    timer = setInterval(function() { $("#btn_next").click();}, 5000);
+                })
+            });
 
 
 
 //首页家装案例和工装案例tab切换
 function change(tab,content){
-var $tab =  $(tab);
-$tab.find("li").eq(0).addClass("active");
-$tab.find("li").on("click",function(){
-var index = $(this).index();
-$(this).addClass("active").siblings().removeClass("active");
-$(content).children("div").eq(index).show().siblings().hide();
-})
+    var $tab =  $(tab);
+    $tab.find("li").eq(0).addClass("active");
+    $tab.find("li").on("click",function(){
+    var index = $(this).index();
+    $(this).addClass("active").siblings().removeClass("active");
+    $(content).children("div").eq(index).show().siblings().hide();
+    })
 }
 change(".tabs",".con");
 
